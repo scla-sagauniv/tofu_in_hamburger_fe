1cfdb53d021f8a7bf31da960ee00cb917a11d2ac
saku-1101 <sakupi1101@outlook.jp>
2023-06-14 16:04:03 +0900

    fix: 真円に修正

--- src/components/atoms/Ingredient.tsx
+++ src/components/atoms/Ingredient.tsx
@@ -4,8 +4,8 @@ import pic from '../../assets/image_png.png';
 export default function Ingredient() {
   return (
     <>
-      <Card elevation={3} style={{ width: '100%', aspectRatio: '1/1', borderRadius: '50%' }}>
-        <Image src={pic} alt='Picture of the author' layout='responsive' />
+      <Card elevation={3} style={{ width: '80%', aspectRatio: '1/1', borderRadius: '50%', position: 'relative' }}>
+        <Image src={pic} alt='Picture of the author' layout='fill' />
       </Card>
     </>
   );
