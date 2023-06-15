b6dce3ed64fc0da7359fd77597bb8a5877f3004f
saku-1101 <sakupi1101@outlook.jp>
2023-06-15 17:28:33 +0900

    add: 受信側の時headerにボタン

--- src/components/molecules/Header.tsx
+++ src/components/molecules/Header.tsx
@@ -1,9 +1,21 @@
 import Title from '../atoms/Title';
 import Switcher from '../atoms/Switcher';
+import GenericButton from '../atoms/GenericButton';
 export default function Header(props: { title: string; isSend: boolean }) {
+  const openLid = () => {
+    console.log('Open the lid');
+    return null;
+  };
   return (
     <>
       <div className='w-full pt-10 pb-20 flex justify-end'>
+        {!props.isSend ? (
+          <div className='w-1/3 flex justify-start'>
+            <GenericButton label='<蓋を開ける' func={openLid} colour='#FEF4EF' />
+          </div>
+        ) : (
+          ''
+        )}
         <Title title={props.title} />
         <Switcher isSend={props.isSend} />
       </div>
