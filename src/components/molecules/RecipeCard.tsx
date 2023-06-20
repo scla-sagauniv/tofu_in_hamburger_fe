ad4fb909fb457c1b8d323c7ed27663f176bd6d8e
shunsuke-tamura <wwwxx4869@gmail.com>
2023-06-20 18:01:14 +0900

    update: とってきたrecipeでうまいこと表示

--- src/components/molecules/RecipeCard.tsx
+++ src/components/molecules/RecipeCard.tsx
@@ -3,10 +3,15 @@ import Image from 'next/image';
 import { Recipe } from '@/gen/ingredientRain_pb';
 
 export default function RecipeCard(props: { recipe: Recipe }) {
+  const openRecipe = (url: string) => {
+    window.open(url, '_blank');
+    console.log(url);
+  };
+
   return (
     <>
       {/* @ts-ignore */}
-      <Card className='p-3' key={index}>
+      <Card className='p-3' key={props.recipe.id}>
         {/* @ts-ignore */}
         <CardHeader
           title={
@@ -19,23 +24,23 @@ export default function RecipeCard(props: { recipe: Recipe }) {
           }
         />
         <div style={{ minHeight: '200px', position: 'relative' }}>
-          <Image src='/tomato_nabe.png' alt='' layout='fill' objectFit='contain' />
+          <Image src={props.recipe.imageUrl} alt='' layout='fill' objectFit='contain' />
         </div>
         {/* @ts-ignore */}
         <CardContent className='h-1/3 overflow-y-auto'>
           <h2 className='text-thick'>おもな材料: {props.recipe.materials}</h2>
-          <h2 className='text-thick'>費用目安: 500円前後</h2>
-          <h2 className='text-thick'>カロリー: 300 kcal</h2>
-          <h2 className='text-thick'>詳細: </h2>
+          <h2 className='text-thick'>費用目安: {props.recipe.recipeCost}</h2>
+          <h2 className='text-thick'>調理時間: {props.recipe.recipeIndication}</h2>
+          {/* <h2 className='text-thick'>詳細: </h2>
           <p className='text-thick text-xs'>
             Lorem ipsum dolor sit amet consectetur, adipisicing elit. Atque architecto reprehenderit magnam esse est id
             ipsum ut delectus. Consequuntur suscipit hic eum ea adipisci, illum sed iure saepe aperiam quia!
-          </p>
+          </p> */}
         </CardContent>
         {/* @ts-ignore */}
         <CardAction className='flex justify-start p'>
           {/* @ts-ignore */}
-          <Button rounded text color='#A492F9'>
+          <Button onClick={() => openRecipe(props.recipe.recipeUrl)} rounded text color='#A492F9'>
             レシピを見る
           </Button>
           {/* @ts-ignore */}
